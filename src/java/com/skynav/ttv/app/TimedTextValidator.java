2d763925f0ed470e38dbf2c9b3c213b469d6f9e8
Glenn Adams <glenn@skynav.com>
2013-05-17 23:33:09 -0600

    Populate with first pass at TTV validator

---
+++ src/java/com/skynav/ttv/app/TimedTextValidator.java
@@ -0,0 +1,232 @@
+/*
+ * Copyright 2013 Skynav, Inc. All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY SKYNAV, INC. AND ITS CONTRIBUTORS “AS IS” AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL SKYNAV, INC. OR ITS CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+ 
+package com.skynav.ttv.app;
+
+import static javax.xml.XMLConstants.W3C_XML_SCHEMA_NS_URI;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.List;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.Unmarshaller;
+import javax.xml.bind.ValidationEvent;
+import javax.xml.bind.ValidationEventHandler;
+import javax.xml.bind.ValidationEventLocator;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
+
+import org.w3c.dom.ls.LSInput;
+import org.w3c.dom.ls.LSResourceResolver;
+import org.xml.sax.ErrorHandler;
+import org.xml.sax.SAXException;
+import org.xml.sax.SAXParseException;
+
+public class TimedTextValidator implements ErrorHandler, LSResourceResolver {
+
+    TimedTextValidator() {
+    }
+
+    public int run(String[] args) {
+        try {
+            return validate(parseArgs(args));
+        } catch (ShowUsageException e) {
+            System.err.println("Usage: ...");
+            return 2;
+        } catch (UsageException e) {
+            System.err.println("Usage: " + e.getMessage());
+            return 2;
+        }
+    }
+
+    // org.xml.sax.ErrorHandler
+
+    public void error(SAXParseException e) {
+        throw new RuntimeException(e.getMessage(), e);
+    }
+
+    public void fatalError(SAXParseException e) {
+        throw new RuntimeException(e.getMessage(), e);
+    }
+
+    public void warning(SAXParseException e) {
+        throw new RuntimeException(e.getMessage(), e);
+    }
+
+    // org.w3c.dom.ls.ResourceResolver
+
+    public LSInput resolveResource(String type, String namespaceURI, String publicId, String systemId, String baseURI) {
+        return null;
+    }
+
+    private URI getCWDAsURI() {
+        try {
+            return new URI("file://" + new File(".").getCanonicalPath().replace(File.separatorChar, '/'));
+        } catch (IOException e) {
+            return null;
+        } catch (URISyntaxException e) {
+            return null;
+        }
+    }
+
+    private List<URL> parseArgs(String[] args) {
+        List<URL> urls = new java.util.ArrayList<URL>();
+        int nonOptionIndex = -1;
+        for (int i = 0; i < args.length;) {
+            String arg = args[i];
+            if (arg.charAt(0) == '-') {
+                switch (arg.charAt(1)) {
+                case '-':
+                    i = parseLongOption(args, i);
+                    break;
+                case '?':
+                    throw new ShowUsageException();
+                default:
+                    if (arg.length() != 2)
+                        throw new UnknownOptionException(arg);
+                    else
+                        i = parseShortOption(args, i);
+                    break;
+                }
+            } else {
+                nonOptionIndex = i;
+                break;
+            }
+        }
+        if (nonOptionIndex >= 0) {
+            URI uriCurrentDirectory = getCWDAsURI();
+            for (int i = nonOptionIndex; i < args.length; ++i) {
+                try {
+                    URI uri = new URI(args[i]);
+                    if (!uri.isAbsolute()) {
+                        URI uriAbsolute = uriCurrentDirectory.resolve(uri);
+                        if (uriAbsolute != null)
+                            uri = uriAbsolute;
+                    }
+                    urls.add(uri.toURL());
+                } catch (URISyntaxException e) {
+                    throw new BadURLArgumentException(e.getMessage());
+                } catch (MalformedURLException e) {
+                    throw new BadURLArgumentException(e.getMessage());
+                }
+            }
+        }
+        return urls;
+    }
+
+    private int parseLongOption(String args[], int index) {
+        return index + 1;
+    }
+
+    private int parseShortOption(String args[], int index) {
+        return index + 1;
+    }
+
+    private Schema createSchema() {
+        try {
+            SchemaFactory sf = SchemaFactory.newInstance(W3C_XML_SCHEMA_NS_URI);
+            sf.setErrorHandler(this);
+            sf.setResourceResolver(this);
+            return sf.newSchema(getClass().getClassLoader().getResource("xsd/ttml10/ttaf1-dfxp.xsd"));
+        } catch (SAXException e) {
+            throw new RuntimeException("can't create schema", e);
+        }
+    }
+    
+    private int validate(URL url) {
+        try {
+            JAXBContext c = JAXBContext.newInstance("com.skynav.ttv.model.ttml10.tt:com.skynav.ttv.model.ttml10.ttm:com.skynav.ttv.model.ttml10.ttp");
+            Unmarshaller u = c.createUnmarshaller();
+            u.setSchema(createSchema());
+            u.setEventHandler(
+                new ValidationEventHandler() {
+                    public boolean handleEvent(ValidationEvent e) {
+                        if (e.getSeverity() != ValidationEvent.WARNING) {
+                            ValidationEventLocator l = e.getLocator();
+                            System.out.println("[" + l.getLineNumber() + ":" + l.getColumnNumber() + "]:" + e.getMessage());
+                        }
+                        return true;
+                    }
+                }
+            );
+            Object tt =  u.unmarshal(url);
+            return (tt != null) ? 0 : 1;
+        } catch (javax.xml.bind.UnmarshalException e) {
+            e.printStackTrace();
+        } catch (javax.xml.bind.JAXBException e) {
+            e.printStackTrace();
+        } catch (RuntimeException e) {
+            e.printStackTrace();
+        }
+        return 1;
+    }
+
+    public int validate(List<URL> urls) {
+        int numFailure = 0;
+        for (URL url : urls) {
+            if (validate(url) != 0)
+                ++numFailure;
+        }
+        return numFailure > 0 ? 1 : 0;
+    }
+
+    public static void main(String[] args) {
+        Runtime.getRuntime().exit(new TimedTextValidator().run(args));
+    }
+
+    private static class UsageException extends RuntimeException {
+        static final long serialVersionUID = 0;
+        UsageException(String message) {
+            super(message);
+        }
+    }
+
+    private static class UnknownOptionException extends UsageException {
+        static final long serialVersionUID = 0;
+        UnknownOptionException(String arg) {
+            super("unknown option: " + arg);
+        }
+    }
+
+    private static class BadURLArgumentException extends UsageException {
+        static final long serialVersionUID = 0;
+        BadURLArgumentException(String arg) {
+            super(arg);
+        }
+    }
+
+    private static class ShowUsageException extends UsageException {
+        static final long serialVersionUID = 0;
+        ShowUsageException() {
+            super("show usage");
+        }
+    }
+
+}
---
+++ tst/java/com/skynav/ttv/app/SimpleValidatorTestCase.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2013 Skynav, Inc. All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY SKYNAV, INC. AND ITS CONTRIBUTORS “AS IS” AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL SKYNAV, INC. OR ITS CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+ 
+package com.skynav.ttv.app;
+
+import java.net.URL;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import com.skynav.ttv.app.TimedTextValidator;
+
+public class SimpleValidatorTestCase {
+    @Test
+    public void testValidatorSimple() throws Exception {
+        URL url = getClass().getResource("ttml10-simple.xml");
+        if (url == null)
+            fail("can't find test resource: ttml10-simple.xml");
+        String[] args = { url.toString() };
+        assertEquals(0, new TimedTextValidator().run(args));
+    }
+
+    @Test
+    public void testValidatorAllElements() throws Exception {
+        URL url = getClass().getResource("ttml10-all-elements.xml");
+        if (url == null)
+            fail("can't find test resource: ttml10-all-elements.xml");
+        String[] args = { url.toString() };
+        assertEquals(0, new TimedTextValidator().run(args));
+    }
+
+    @Test
+    public void testValidatorAllStyles() throws Exception {
+        URL url = getClass().getResource("ttml10-all-styles.xml");
+        if (url == null)
+            fail("can't find test resource: ttml10-all-styles.xml");
+        String[] args = { url.toString() };
+        assertEquals(0, new TimedTextValidator().run(args));
+    }
+}
+
