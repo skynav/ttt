e69ec650345a793317357f05b18c78b91c6304b3
Glenn Adams <glenn@skynav.com>
2015-09-28 17:22:13 -0600

    [*] finalize release 2 artifact staging

--- ttt-deps/ttt-fontbox/deploy-staging.sh
+++ ttt-deps/ttt-fontbox/deploy-staging.sh
@@ -4,6 +4,6 @@ mvn gpg:sign-and-deploy-file \
   -Durl=https://oss.sonatype.org/service/local/staging/deploy/maven2/ \
   -DrepositoryId=ossrh \
   -DpomFile=pom.xml \
-  -Dfile=ttt-fontbox-1.0.jar \
-  -Dsources=ttt-fontbox-1.0-sources.jar \
-  -Djavadoc=ttt-fontbox-1.0-javadoc.jar
+  -Dfile=ttt-fontbox-2.0.jar \
+  -Dsources=ttt-fontbox-2.0-sources.jar \
+  -Djavadoc=ttt-fontbox-2.0-javadoc.jar
