be7403892acfc95ae8c569d1d2db6f1a7a303641
Glenn Adams <glenn@skynav.com>
2018-08-21 18:28:43 -0600

    [ttpe] skip opacity presentation tests temporarily

--- ttt-ttpe/src/test/java/com/skynav/ttpe/w3c/ttml2/presentation/valid/TTML2PresenterTestCases.java
+++ ttt-ttpe/src/test/java/com/skynav/ttpe/w3c/ttml2/presentation/valid/TTML2PresenterTestCases.java
@@ -260,19 +260,19 @@ public class TTML2PresenterTestCases extends PresenterTestDriver {
     }
 
     @Test
-    //@Ignore // pending https://github.com/skynav/ttt/issues/208
+    @Ignore // pending https://github.com/skynav/ttt/issues/208
     public void testTTML2OpacityBlock() throws Exception {
         performPresentationTest("ttml2-prstn-opacity-block.xml", 0, 0);
     }
 
     @Test
-    //@Ignore // pending https://github.com/skynav/ttt/issues/208
+    @Ignore // pending https://github.com/skynav/ttt/issues/208
     public void testTTML2OpacityInline() throws Exception {
         performPresentationTest("ttml2-prstn-opacity-inline.xml", 0, 0);
     }
 
     @Test
-    //@Ignore // pending https://github.com/skynav/ttt/issues/208
+    @Ignore // pending https://github.com/skynav/ttt/issues/208
     public void testTTML2OpacityRegion() throws Exception {
         performPresentationTest("ttml2-prstn-opacity-region.xml", 0, 0);
     }
